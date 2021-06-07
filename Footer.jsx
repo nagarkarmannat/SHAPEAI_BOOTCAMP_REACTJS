6adf346e39906da6c1a81b58aa4be958fe7d69e4
nagarkarmannat <66298022+nagarkarmannat@users.noreply.github.com>
2021-06-07 11:51:59 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Footer() {
+  var curryear = new Date().getFullYear();
+
+  return (
+          <footer>
+            <p>copyright @ {curryear}</p>
+          </footer>
+  );
+}
+
+
+
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+          <div className ="note">
+              <h1> JAVA SCRIPT AND REACT.js </h1>
+              <p>I ve attend this Bootcamp for 7 days. I learnt alot from this Bootcamp.</p>
+          </div>
+  );
+}
+
+
+
+export default Note;
